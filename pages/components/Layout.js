3ce2aa0ceabcbb32240f8fbc6ce13ea5e5936d57
Boniface M <bonfacemugweh@gmail.com>
2022-05-18 07:38:03 +0300

    Getting started

---
+++ pages/components/Layout.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Layout.css'
+
+const Layout = () => {
+  return (
+    <div className='{styles.container}'>
+        <main className={styles.main}>
+
+        </main>
+
+    </div>
+  )
+}
+
+export default Layout
